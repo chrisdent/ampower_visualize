1d1a43fb275e898c6d5fa1a769fff6b8a1e435fa
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2024-12-10 13:13:37 +0530

    item level implementation using D3.js (#5)
    * changes in color scheme and canvas width
    * adds business logic to visualization (#3)
    * added basic skeleton and backend functions for first level sales order
    * made some structural changes to UI and backend functions that implements business logic
    * added items table in each node of the document tree, changes function names, colors, transitions
    * added docstatus flag, special case of SO to PO
    * added custom doctypes
    * modified backend function to fetch item level data in json format (#4)
    * modified backend function to fetch item level data in json format
    * created fixed hierarchy for child-parent relationships
    * fixed graph to dissociate based on parent
    * removed redundant nodes
    * changed shape to squares, added document links from canvas
    * created legend for node colors
    * formatted backend file, changed script to fetch only documents that are not cancelled
    * fixed cluttered nodes, shades, parameters on label, added text on links
    * added future scope, tutorial, features and screenshots to readme
    * created common function for get_all, convert indents to tabs
    * Updated readme and toml with stable versions (#6)
    * added future scope, fix for issues while installation
    * updated version in pyproject.toml
    * removed dependency names from comments
    
    ---------
    
    Reviewed-by: Amit Gandhi <amit.gandhi@ambibuzz.com>
    Signed-off-by: Sambhav Saxena <sambhav.saxena@ambibuzz.com>

--- ampower_visualize/ampower_visualize/page/product_traceability/product_traceability.js
+++ ampower_visualize/ampower_visualize/page/product_traceability/product_traceability.js
@@ -32,6 +32,9 @@ const setup_fields = (page, wrapper) => {
 		fieldtype: 'Link',
 		fieldname: 'document_type',
 		options: 'DocType',
+		filters: {
+			name: ["in", ["Sales Order"]]
+		},
 		change() {
 			const doctype = doctype_field.get_value();
 			if (doctype && doctype !== previous_doctype_name) {
@@ -46,6 +49,13 @@ const setup_fields = (page, wrapper) => {
 		fieldtype: 'Link',
 		fieldname: 'document',
 		options: previous_doctype_name,
+		get_query() {
+			return {
+				filters: {
+					docstatus: 1
+				}
+			};
+		},
 		change() {
 			const document_name = document_field.get_value();
 			if (document_name && document_name !== previous_document_name) {
@@ -82,53 +92,7 @@ const update_visualization = (wrapper, doctype, document_name) => {
  */
 const append_static_html = () => {
 	$(global_wrapper).find('.layout-main-section').append(`
-		<script>
-			let isDragging = false;
-			let startX, startY;
-			let offsetX = 0, offsetY = 0;
-			let scale = 1;
-			const minScale = 0.1;
-			const maxScale = 5;
-			const updateTransform = () => {
-				document.querySelector('.tree').style.transform = \`translate(\${ offsetX }px, \${ offsetY }px) scale(\${ scale })\`;
-			}
-			const handleMouseDown = (e) => {
-				if (e.target.tagName.toLowerCase() === 'a') return; // Disable drag when clicking on a link
-				isDragging = true;
-				startX = e.clientX - offsetX;
-				startY = e.clientY - offsetY;
-				document.getElementById('canvas-container').style.cursor = 'grabbing';
-			}
-			const handleMouseMove = (e) => {
-				if (isDragging) {
-					offsetX = e.clientX - startX;
-					offsetY = e.clientY - startY;
-					updateTransform();
-				}
-			}
-			const handleMouseUp = () => {
-				isDragging = false;
-				document.getElementById('canvas-container').style.cursor = 'move';
-			}
-
-			const handleWheel = (e) => {
-				e.preventDefault();
-				const delta = e.deltaY > 0 ? 0.9 : 1.1;
-				const newScale = Math.min(Math.max(scale * delta, minScale), maxScale);
-				
-				// Calculate mouse position relative to the tree
-				const rect = document.querySelector('.tree').getBoundingClientRect();
-				const mouseX = e.clientX - rect.left;
-				const mouseY = e.clientY - rect.top;
-				
-				// Adjust offset to zoom towards mouse position
-				offsetX += mouseX * (1 - delta);
-				offsetY += mouseY * (1 - delta);
-
-				scale = newScale;
-				updateTransform();
-			}
-		</script>
+		<script src="https://d3js.org/d3.v7.min.js"/>
 	`);
 }
 
@@ -141,120 +105,356 @@ const append_static_html = () => {
  */
 const append_dynamic_html = (doctype, document_name) => {
 	if (!doctype) {
-		show_alert("No doctype specified");
+		notify("No doctype specified");
 		return;
 	}
 	if (!document_name) {
-		show_alert("No document name specified");
+		notify("No document name specified");
 		return;
 	}
 	$(global_wrapper).find('.layout-main-section').append(`
 		<div class="top-level-parent">
 			<script>
-				document.getElementById('canvas-container').addEventListener('mousedown', handleMouseDown);
-				document.getElementById('canvas-container').addEventListener('mousemove', handleMouseMove);
-				document.getElementById('canvas-container').addEventListener('mouseup', handleMouseUp);
-				document.getElementById('canvas-container').addEventListener('mouseleave', handleMouseUp);
-				document.getElementById('canvas-container').addEventListener('wheel', handleWheel);
-				document.querySelector('.tree').style.transformOrigin = '0 0';
-				updateTransform();
-				refresh_list_properties();
+				configure_query_url('${doctype}', '${document_name}');
 			</script>
-			<style>
-				.layer-wrapper{display:flex;align-items:center;justify-content:center;margin-top:5vh}#canvas-container{width:1000px;height:600px;background-color:#005ce6;border:1px solid #000;box-shadow:0 0 10px rgb(0 0 0 / .1);overflow:hidden;cursor:move}#canvas{position:relative;width:9999px;height:9999px}.tree{width:9999px;height:9999px}.tree ul{padding-top:20px;position:relative;transition:.5s}.tree li{display:inline-table;text-align:center;color:#fff;list-style-type:none;position:relative;padding:10px;transition:.5s}.tree li::before,.tree li::after{content:'';position:absolute;top:0;right:50%;border-top:1px solid #000;width:51%;height:10px}.tree li::after{right:auto;left:50%;border-left:1px solid #000}.tree li:only-child::after,.tree li:only-child::before{display:none}.tree li:only-child{padding-top:0}.tree li:first-child::before,.tree li:last-child::after{border:0 none}.tree li:last-child::before{border-right:1px solid #000;border-radius:0 5px 0 0;-webkit-border-radius:0 5px 0 0;-moz-border-radius:0 5px 0 0}.tree li:first-child::after{border-radius:5px 0 0 0;-webkit-border-radius:5px 0 0 0;-moz-border-radius:5px 0 0 0}.tree ul ul::before{content:'';position:absolute;top:0;left:50%;border-left:1px solid #000;width:0;height:20px}.tree li a{border:1px solid #000;padding:10px;display:inline-grid;border-radius:5px;text-decoration-line:none;border-radius:5px;transition:.5s}.tree li a span{border:1px solid #000;border-radius:5px;color:#000;padding:8px;font-size:12px;text-transform:uppercase;letter-spacing:1px;font-weight:500}.tree li a:hover,.tree li a:hover i,.tree li a:hover span,.tree li a:hover+ul li a{background-color:#ED9226;border:1px solid #000}.tree li a:hover+ul li::after,.tree li a:hover+ul li::before,.tree li a:hover+ul::before,.tree li a:hover+ul ul::before{border-color:#fff}.tree>ul{display:block}.tree ul ul{display:none}.tree ul ul.active{display:block}
-			</style>
-			<div class="layer-wrapper">
-				<div id="canvas-container">
-					<div id="canvas">
-						<div class="tree">
-							<ul>
-								<li class="${document_name}">
-									<a onclick="append_linked_nodes('${doctype}', '${document_name}')"><b>${doctype} - ${document_name}</b></a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
 		</div>
 	`);
 }
 
 /**
- * Refreshes the list properties for each list item on the canvas
- */
-const refresh_list_properties = () => {
-	const togglerLinks = document.querySelectorAll(".tree a");
-	togglerLinks.forEach(link => {
-		link.addEventListener("click", function (event) {
-			event.preventDefault();
-			const childUl = this.nextElementSibling;
-			if (childUl) {
-				childUl.classList.toggle("active");
-				this.classList.toggle("expanded");
-			}
-		});
-	});
-}
-
-/**
- * takes the doctype and document_name as parameters and returns a list of links to that document
- * then, this list is iterated and a child node is created for each link
- * these children are then clubbed into an HTML ul, and appended to the base HTML on canvas
+ * configures corresponding backend functions depending on the doctype
  * @param {String} doctype
  * @param {String} document_name
  */
-const append_linked_nodes = (doctype, document_name) => {
+const configure_query_url = (doctype, document_name) => {
 	if (!doctype || !document_name) {
-		show_alert("Error parsing fields.", "red")
+		notify("Error parsing fields.", "red");
 		return;
 	}
-	const nodeElement = document.querySelector(`.${document_name}`);
-	if (!nodeElement.isExpanded) {
-		nodeElement.isExpanded = false;
+	let method_type = 'ampower_visualize.ampower_visualize.page.product_traceability.product_traceability.';
+	switch (doctype) {
+		case 'Sales Order':
+			method_type += 'get_sales_order_links';
+			break;
+		default:
+			notify("This is the last node.", "red", 5);
+			return;
 	}
-	else return;
+	const node_element = document.querySelector(`.top-level-parent`);
+	get_graph_data(document_name, method_type, node_element);
+}
+
+/**
+ * Fetches data from the backend function, formats it into a JSON that can be consumed directly by the library functions
+ * @param {String} document_name
+ * @param {String} method_type
+ * @param {DOM Element} node_element
+ */
+const get_graph_data = (document_name, method_type, node_element) => {
 	frappe.call({
-		method: 'ampower_visualize.ampower_visualize.page.product_traceability.product_traceability.get_linked_documents',
-		args: {
-			doctype: doctype,
-			docname: document_name
-		},
+		method: method_type,
+		args: { document_name: document_name },
 		callback: function (r) {
-			if (!r.message.length) {
-				show_alert("Node cannot be expanded further.", "red");
+			if (!r.message || r.message.length === 0) {
+				notify("Invalid data format or no items to display.", "red");
 				return;
 			}
-			const new_list = document.createElement("ul");	// creates a new list and populates it with list items (links to documents)
-			new_list.className = "active";
-			for (let i = 0; i < r.message.length; i++) {
-				const new_item = document.createElement("li");
-				new_item.className = r.message[i].linked_parent;
-				const new_link = document.createElement("a");
-				new_link.innerHTML = `
-					${r.message[i].linked_parent} <br/> 
-					Type: ${r.message[i].linked_parenttype} <br/> 
-					Created on: ${r.message[i].date.split(' ')[0]}
-				`;
-				new_link.onclick = () => {
-					append_linked_nodes(r.message[i].linked_parenttype, r.message[i].linked_parent);
+
+			const data = r.message.items;
+			const graph_data = { nodes: [], links: [] };
+			const existing_nodes = new Set();
+
+			data.forEach((item) => {
+				const parent_node_id = `${item.item_code}-${item.sales_order_qty}`;
+				if (!existing_nodes.has(parent_node_id)) {
+					graph_data.nodes.push({
+						id: parent_node_id,
+						label: `${item.item_name}\n(${item.item_code})`,
+						type: 'sales_order_item',
+						qty: item.sales_order_qty,
+						is_parent: true,
+						expanded: false
+					});
+					existing_nodes.add(parent_node_id);
 				}
-				new_item.appendChild(new_link);
-				new_list.appendChild(new_item);
+
+				const add_connections = (connections, type) => {
+					connections.forEach(connection => {
+						const child_node_id = connection.unique_id;
+
+						if (!existing_nodes.has(child_node_id)) {
+							graph_data.nodes.push({
+								id: child_node_id,
+								label: `${connection[type]}`,
+								type: type,
+								qty: connection.qty,
+								status: connection.status,
+								is_parent: false
+							});
+							existing_nodes.add(child_node_id);
+						}
+
+						graph_data.links.push({
+							source: parent_node_id,
+							target: child_node_id
+						});
+					});
+				};
+
+				add_connections(item.sales_invoices, "sales_invoice");
+				add_connections(item.delivery_notes, "delivery_note");
+				add_connections(item.material_requests, "material_request");
+				add_connections(item.purchase_orders, "purchase_order");
+			});
+
+			const root_node_id = "root";
+			if (!existing_nodes.has(root_node_id)) {
+				graph_data.nodes.push({
+					id: root_node_id,
+					label: document_name,
+					type: "root",
+					qty: '',
+					is_parent: true,
+					expanded: false,
+				});
+				existing_nodes.add(root_node_id);
 			}
-			$(global_wrapper).find(`.${document_name}`).append(new_list);
-			nodeElement.isExpanded = true;
-			refresh_list_properties();	// sets properties to newly added list items
+
+			const connectedNodeIds = new Set(graph_data.links.map(link => link.target));
+			graph_data.nodes.forEach(node => {
+				if (!connectedNodeIds.has(node.id) && node.id !== root_node_id) {
+					graph_data.links.push({
+						source: root_node_id,
+						target: node.id
+					});
+				}
+			});
+
+			visualize_graph(graph_data, node_element);
 		},
 		freeze: true,
 		freeze_message: __("Fetching linked documents...")
 	});
-}
+};
 
 /**
- * Utility function to call frappe alerts
+ * Creates a graph using the JSON data and appends it to the root node
+ * @param {Dict} graph_data
+ * @param {DOM Element} node_element
  */
-const show_alert = (message, indicator = "yellow", time = 3) => {	// default time and indicators set
+const visualize_graph = (graph_data, node_element) => {
+	const width = 1256, height = 720;
+	d3.select(node_element).select("svg").remove();
+
+	const svg = d3.select(node_element)
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.call(
+			d3.zoom()
+				.scaleExtent([0.1, 3])
+				.on("zoom", event => {
+					g.attr("transform", event.transform);
+				})
+		)
+		.append("g");
+
+	const g = svg.append("g");
+
+	const parentNodes = graph_data.nodes.filter(node => node.is_parent);
+
+	const nodeColors = {
+		'sales_order_item': '#ff59d0',
+		'sales_invoice': '#3498db',
+		'delivery_note': '#e74c3c',
+		'material_request': '#f39c12',
+		'purchase_order': '#2ecc71',
+		'purchase_invoice': '#9b59b6',
+		'purchase_receipt': '#34495e',
+		'root': '#b0b336'
+	};
+
+	const nodeSizes = {
+		'sales_order_item': 60,
+		'default': 36
+	};
+
+	const legendData = [
+		{ type: 'root', label: 'Root Document' },
+		{ type: 'sales_order_item', label: 'Sales Order Item' },
+		{ type: 'sales_invoice', label: 'Sales Invoice' },
+		{ type: 'delivery_note', label: 'Delivery Note' },
+		{ type: 'material_request', label: 'Material Request' },
+		{ type: 'purchase_order', label: 'Purchase Order' },
+		{ type: 'purchase_invoice', label: 'Purchase Invoice' },
+		{ type: 'purchase_receipt', label: 'Purchase Receipt' }
+	];
+
+	const legendGroup = svg.append("g")
+		.attr("transform", `translate(20, 10)`);
+
+	const legendItems = legendGroup.selectAll(".legend-item")
+		.data(legendData)
+		.enter()
+		.append("g")
+		.attr("transform", (d, i) => `translate(${i * 150}, 0)`);
+
+	legendItems.append("rect")
+		.attr("width", 20)
+		.attr("height", 20)
+		.attr("fill", d => nodeColors[d.type] || '#69b3a2')
+
+	legendItems.append("text")
+		.attr("x", 25)
+		.attr("y", 15)
+		.text(d => d.label)
+		.style("fill", "#555555")
+		.style("font-size", "12px");
+
+	const format_document_url = (base_url, type, label) => {
+		let formatted_label, document_name;
+		if (type === "root") {
+			formatted_label = "sales-order";
+		}
+		else if (type === "sales_order_item") {
+			formatted_label = "item";
+		}
+		else formatted_label = type.replace(/_/g, '-');
+		if (type === "sales_order_item") {
+			document_name = label.match(/\((.*?)\)/)[1];
+		}
+		else document_name = label.split(' ')[0];
+		return `${base_url}/app/${formatted_label}/${document_name}`;
+	}
+
+	const simulation = d3.forceSimulation(graph_data.nodes)
+		.force("link", d3.forceLink(graph_data.links)
+			.id(d => d.id)
+			.distance(300)
+		)
+		.force("charge", d3.forceManyBody()
+			.strength(-100)
+		)
+		.force("center", d3.forceCenter(width / 2, height / 2))
+		.force("parent_repulsion", d => {
+			for (let i = 0; i < graph_data.nodes.length; i++) {
+				if (!graph_data.nodes[i].is_parent) {
+					for (let j = 0; j < parentNodes.length; j++) {
+						const parent = parentNodes[j];
+						const node = graph_data.nodes[i];
+						
+						const dx = node.x - parent.x;
+						const dy = node.y - parent.y;
+						const distance = Math.sqrt(dx * dx + dy * dy);
+						
+						const strength = 0.5;
+						node.vx += dx / distance * strength;
+						node.vy += dy / distance * strength;
+					}
+				}
+			}
+		})
+		.alphaDecay(0)
+		.alphaMin(0.001)
+		.alphaTarget(0.3);
+
+	const link = g.append("g")
+		.selectAll("line")
+		.data(graph_data.links)
+		.enter()
+		.append("g");
+
+		link.append("line")
+		.attr("stroke", "#696C71")
+		.attr("stroke-width", d => d.source.is_parent ? 2.5 : 1.5)
+		.attr("stroke-opacity", 0.6);
+
+		link.append("text")
+		.attr("text-anchor", "end")
+		.style("font-size", "14px")
+		.style("fill", "#696C71")
+		.text(d => `${d.target.status ? d.target.status + " [Qty: " + d.target.qty + "]" : ''}`);
+
+	const node = g.append("g")
+		.selectAll("rect")
+		.data(graph_data.nodes)
+		.enter()
+		.append("rect")
+		.attr("width", d => d.is_parent ? nodeSizes['sales_order_item'] : nodeSizes['default'])
+		.attr("height", d => d.is_parent ? nodeSizes['sales_order_item'] : nodeSizes['default'])
+		.attr("fill", d => nodeColors[d.type] || '#69b3a2')
+		.attr("x", d => d.x - (d.is_parent ? nodeSizes['sales_order_item'] : nodeSizes['default']) / 2)
+		.attr("y", d => d.y - (d.is_parent ? nodeSizes['sales_order_item'] : nodeSizes['default']) / 2)
+		.call(d3.drag()
+			.on("start", dragstarted)
+			.on("drag", dragged)
+			.on("end", dragended));
+
+	const label = g.append("g")
+		.selectAll("a")
+		.data(graph_data.nodes)
+		.enter()
+		.append("a")
+		.attr("xlink:href", d => format_document_url(window.location.origin, d.type, d.label))
+		.attr("target", "_blank")
+		.append("text")
+		.text(d => d.label)
+		.style("font-size", d => d.is_parent ? "12px" : "10px")
+		.style("font-weight", d => d.is_parent ? "bold" : "normal")
+		.style("fill", d => "#000")
+		.attr("text-anchor", "middle")
+		.attr("alignment-baseline", "middle");
+
+	simulation.on("tick", () => {
+		link
+			.attr("x1", d => d.source.x)
+			.attr("y1", d => d.source.y)
+			.attr("x2", d => d.target.x)
+			.attr("y2", d => d.target.y);
+		
+		link.selectAll("line")
+			.attr("x1", d => d.source.x)
+			.attr("y1", d => d.source.y)
+			.attr("x2", d => d.target.x)
+			.attr("y2", d => d.target.y);
+
+		link.selectAll("text")
+			.attr("x", d => (d.source.x + d.target.x) / 2)
+			.attr("y", d => (d.source.y + d.target.y) / 2);
+
+		node
+			.attr("x", d => d.x - (d.is_parent ? nodeSizes['sales_order_item'] : nodeSizes['default']) / 2)
+			.attr("y", d => d.y - (d.is_parent ? nodeSizes['sales_order_item'] : nodeSizes['default']) / 2);
+
+		label
+			.attr("x", d => d.x)
+			.attr("y", d => d.y);
+	});
+
+	function dragstarted(event, d) {
+		if (!event.active) simulation.alphaTarget(0.3).restart();
+		d.fx = d.x;
+		d.fy = d.y;
+	}
+
+	function dragged(event, d) {
+		d.fx = event.x;
+		d.fy = event.y;
+	}
+
+	function dragended(event, d) {
+		if (!event.active) simulation.alphaTarget(0);
+		d.fx = null;
+		d.fy = null;
+	}
+};
+
+/**
+ * UTILITY FUNCTIONS
+ */
+
+// Sends frappe alerts
+const notify = (message, indicator = "yellow", time = 3) => {	// default time and indicators set
 	frappe.show_alert({
 		message: __(message),
 		indicator: indicator
